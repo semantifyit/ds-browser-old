f035688c944dcff15156d0920b86bc3a1158cd0c
omar <toshiro_umezawa99@yahoo.de>
2019-02-14 12:53:29 +0100

    show desc and types in DS list table

--- scripts/propertyRowConstructor.js
+++ scripts/propertyRowConstructor.js
@@ -76,7 +76,7 @@ function createHTMLForDSList(dsListObj) {
     var arr = [];
     var keys = Object.keys(dsListObj);
     for (var i = 0; i < keys.length; i++) {
-        if (dsListObj[keys[i]]["isInstantAnnotation"] === false && !dsListObj[keys[i]]['name'].startsWith("Broker_")) {
+        if (dsListObj[keys[i]]["isInstantAnnotation"] === false && !dsListObj[keys[i]]['name'].startsWith("Broker") && !dsListObj[keys[i]]['name'].startsWith("Simple") && !dsListObj[keys[i]]['name'].startsWith("schema")) {
             if (dsListObj[keys[i]]['name'].charAt(0).toUpperCase() !== dsListObj[keys[i]]['name'].charAt(0)) {
                 dsListObj[keys[i]]['name'] = dsListObj[keys[i]]['name'].charAt(0).toUpperCase().concat(dsListObj[keys[i]]['name'].substring(1))
                 console.log(dsListObj[keys[i]]['name']);
@@ -84,13 +84,46 @@ function createHTMLForDSList(dsListObj) {
             arr.push(dsListObj[keys[i]]);
         }
     }
+
     arr = sortByKeyAsc(arr, "name");
     for (var i = 0; i < arr.length; i++) {
         var name = arr[i]['name'];
         var hash = arr[i]['hash'];
+        var desc = "";
+        if (arr[i]['description'] !== undefined) {
+            desc = arr[i]['description'];
+        }
+        var types = uniquifyArray(arr[i]['types']);
+        var typesCode = "<td class=\"prop-types\">" + createTypesCode(hash, types) + "</td>";
+        var descCode = "<td class=\"prop-desc\">" + repairLinksInHTMLCode(desc) + "</td>";
         var newUrl = location.href.concat("?ds=" + hash);
         var linkCode = "<a href='" + newUrl + "'>" + name + "</a>";
-        code = code.concat("<tr><th class=\"prop-nam\"><code property=\"rdfs:label\">" + linkCode + "</code></th></tr>");
+        code = code.concat("<tr><th class=\"prop-nam\"><code property=\"rdfs:label\">" + linkCode + "</code></th>" + typesCode + descCode + "</tr>");
+
+    }
+    return code;
+}
+
+//make sure each URL is only 1 time in the array
+function uniquifyArray(array) {
+    let seen = {};
+    let result = [];
+    for (let i = 0; i < array.length; i++) {
+        let item = array[i];
+        if (seen[item] !== 1) {
+            seen[item] = 1;
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+function createTypesCode(hash, arr) {
+    var code = "";
+    for (var i = 0; i < arr.length; i++) {
+        var name = arr[i];
+        var newUrl = location.href.concat("?ds=" + hash + "&path=" + name);
+        code = code.concat("<a href='" + newUrl + "'>" + name + "</a><br>");
 
     }
     return code;
