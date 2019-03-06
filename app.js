4176c8b81b6f3abd8fc328ea5bf06a99b5034cee
Roland Gritzer <roland@kolm.uibk.ac.at>
2019-03-06 15:07:35 +0100

    fixed space

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(function (req, res, next) {
 
 app.use(express.static('public'));
 
-app.get('/*', (req, res) = > {
+app.get('/*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
